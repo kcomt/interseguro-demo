701ab9c5a88efebf88375df5da3e88c3645bc5c8
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2022-08-16 16:33:11 -0500

    Creacion de proyecto

---
+++ src/main/java/com/kcomt/interseguro/InterseguroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kcomt.interseguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterseguroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterseguroDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcomt/interseguro/InterseguroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcomt.interseguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterseguroDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
